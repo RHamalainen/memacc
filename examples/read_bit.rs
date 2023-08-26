fb40aa6919ea76d275e90017ca0ceb296d11ac6d
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2023-08-26 18:47:48 +0300

    Remove examples

--- examples/read_bit.rs
+++
@@ -1,9 +0,0 @@
-use memacc::bitman::ReadBit;
-
-fn main() {
-    let value = 0b1010_0110u8;
-    for index in 0..=7 {
-        let state = value.read_bit(index);
-        println!("State at index {index} is {state}.");
-    }
-}
