df997dd9246ecc8ca141c97cbe9b19e7a550a4e5
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2023-09-07 15:31:35 +0300

    Implement SetAddressBit for other pointer widths

--- src/memman/set_address_bit.rs
+++ src/memman/set_address_bit.rs
@@ -1,7 +1,6 @@
 use crate::bitman::SetBit;
-
-use super::ReadFromAddress;
-use super::WriteToAddress;
+use crate::memman::ReadFromAddress;
+use crate::memman::WriteToAddress;
 
 pub trait SetAddressBit {
     type Type;
@@ -9,12 +8,24 @@ pub trait SetAddressBit {
     fn set_address_bit(self, index: Self::Index);
 }
 
-impl SetAddressBit for *mut u32 {
-    type Type = *mut u32;
-    type Index = u32;
-    fn set_address_bit(self, index: Self::Index) {
-        let value_old = self.read_from_address();
-        let value_new = value_old.set_bit(index);
-        self.write_to_address(value_new);
-    }
+/// Implement [`SetAddressBit`] for given type.
+macro_rules! ImplementSetAddressBit {
+    ($type:ty) => {
+        impl SetAddressBit for *mut $type {
+            type Type = *mut $type;
+            type Index = $type;
+            fn set_address_bit(self, index: Self::Index) {
+                let value_old = self.read_from_address();
+                let value_new = value_old.set_bit(index);
+                self.write_to_address(value_new);
+            }
+        }
+    };
 }
+
+ImplementSetAddressBit!(u8);
+// TODO: ImplementSetAddressBit!(u16);
+ImplementSetAddressBit!(u32);
+ImplementSetAddressBit!(u64);
+
+// TODO: tests
