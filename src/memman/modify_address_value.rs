630728d326d03d5eb1e37425afc96d9250d5cdce
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2023-09-07 16:44:01 +0300

    Update memman

--- src/memman/modify_address_value.rs
+++ src/memman/modify_address_value.rs
@@ -2,19 +2,24 @@ use super::ReadFromAddress;
 use super::WriteToAddress;
 
 pub trait ModifyAddressValue {
-    type Type;
-    #[deprecated = "does not work as expected"]
-    fn modify_address_value(self, f: fn(Self::Type) -> Self::Type);
+    type Word;
+    fn modify_address_value<F>(self, f: F)
+    where
+        F: FnOnce(&Self::Word) -> Self::Word;
 }
 
 /// Implement [`ModifyAddressValue`] for given type.
 macro_rules! ImplementModifyAddressValue {
     ($type:ty) => {
         impl ModifyAddressValue for *mut $type {
-            type Type = $type;
-            fn modify_address_value(self, f: fn(Self::Type) -> Self::Type) {
+            type Word = $type;
+            #[inline]
+            fn modify_address_value<F>(self, f: F)
+            where
+                F: FnOnce(&Self::Word) -> Self::Word,
+            {
                 let value_old = self.read_from_address();
-                let value_new = f(value_old);
+                let value_new = f(&value_old);
                 self.write_to_address(value_new);
             }
         }
@@ -26,4 +31,49 @@ ImplementModifyAddressValue!(u16);
 ImplementModifyAddressValue!(u32);
 ImplementModifyAddressValue!(u64);
 
-// TODO: tests
+#[cfg(test)]
+mod tests {
+    use crate::bitman::SetBit;
+    use crate::bitman::SetBits;
+    use crate::memman::ReadFromAddress;
+
+    use super::ModifyAddressValue;
+
+    #[test]
+    fn test_modify_address_value_0() {
+        let mut value = 0b0000_0000u8;
+        let reference = &mut value;
+        let address = reference as *mut u8;
+        address.modify_address_value(|w| w.set_bit(0));
+        assert_eq!(address.read_from_address(), 0b0000_0001);
+    }
+
+    #[test]
+    fn test_modify_address_value_1() {
+        let mut value = 0b0000_0000u8;
+        let reference = &mut value;
+        let address = reference as *mut u8;
+        address.modify_address_value(|w| w.set_bits(0..=1));
+        assert_eq!(address.read_from_address(), 0b0000_0011);
+    }
+
+    #[test]
+    fn test_modify_address_value_2() {
+        let mut value = 0b0000_0000u8;
+        let reference = &mut value;
+        let address = reference as *mut u8;
+        let action = |w: &u8| w.set_bit(0);
+        address.modify_address_value(action);
+        assert_eq!(address.read_from_address(), 0b0000_0001);
+    }
+
+    #[test]
+    fn test_modify_address_value_3() {
+        let mut value = 0b0000_0000u8;
+        let reference = &mut value;
+        let address = reference as *mut u8;
+        let action = |w: &u8| w.set_bits(0..=1);
+        address.modify_address_value(action);
+        assert_eq!(address.read_from_address(), 0b0000_0011);
+    }
+}
